436e9044751cdfd3c01e3107ade0a734b3913cee
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-05-05 21:14:32 +0200

    Geanina: Added routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import { withNamespaces } from 'react-i18next';
 import i18n from './i18n';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 class App extends Component {
 
@@ -9,13 +11,18 @@ class App extends Component {
     i18n.changeLanguage(lng);
   }
 
-  render(){
-    const {t} = this.props;
-    return(
+  render() {
+    const { t } = this.props;
+    return (
       <div>
-      <button onClick={() => this.changeLanguage('fr')}>fr</button>
-      <button onClick={() => this.changeLanguage('en')}>en</button>
-        {t('title')} 
+        <button onClick={() => this.changeLanguage('fr')}>fr</button>
+        <button onClick={() => this.changeLanguage('en')}>en</button>
+        {t('title')}
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; 
+
+class Login extends Component {
+    render(){
+        return(
+            <div>
+                ha ha ha 
+            </div>
+        )
+    }
+}
+export default Login; 
